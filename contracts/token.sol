62fa9cc762c11aa97bd40807d6be781cd92834d8
lehyichen1998 <lehyichen1998@gmail.com>
2024-07-18 16:09:05 +0800

    push code

--- contracts/token.sol
+++ contracts/token.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.10.0;
+pragma solidity ^0.8.19;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 contract TheodoresToken is ERC20("Theodores Token", "TT"),Ownable(msg.sender){
     
-    function mintFifty(uint16 num_token) public onlyOwner{
-        _mint(msg.sender, num_token* 10**18);
+    function mintFifty(uint256 num_token) public onlyOwner{
+        _mint(msg.sender, num_token*10**18);
     }
 }
